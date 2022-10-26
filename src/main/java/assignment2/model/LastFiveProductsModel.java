44606b8d890a53842c3b3679d2b205644c692c61
Kylie Guo <kguo5959@uni.sydney.edu.au>
2022-10-26 22:14:55 +1100

    finished tests and changed last five products logic

--- src/main/java/assignment2/model/LastFiveProductsModel.java
+++ src/main/java/assignment2/model/LastFiveProductsModel.java
@@ -1,21 +1,43 @@
 package assignment2.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LastFiveProductsModel {
+
+    public static boolean containsProductName(String name, List<Purchase> purchases){
+
+        for (Purchase p : purchases){
+            if (p.getItem().equals(name)){
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public List<Purchase> getLastFiveProductsBoughtByUser(User user){
 
         int size = user.getPurchases().size();
 
-        if (size <= 5){
-            // get all the  purchases
-            return user.getPurchases();
-        } else {
+        List<Purchase> lastFiveProducts = new ArrayList<Purchase>();
 
-            // get the last 5 purchases from list
-            return user.getPurchases().subList(size - 5, size);
+        for (int i = size - 1; i >= 0; i--){
+
+            if (lastFiveProducts.size() >= 5){
+                break;
+            }
+
+            Purchase current = user.getPurchases().get(i);
+
+            if (!containsProductName(current.getItem(), lastFiveProducts)){
+                lastFiveProducts.add(current);
+            }
+
         }
+
+        return lastFiveProducts;
+
 
     }
 }
