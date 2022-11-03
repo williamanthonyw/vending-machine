3f4187fb9efc414537b6c95dc4eeb6afab8faed0
William Anthony <wwid6807@uni.sydney.edu.au>
2022-11-03 15:27:26 +1100

    edit view and fixed tests overall 85%

--- src/test/java/assignment2/CSVFileParserTest.java
+++ src/test/java/assignment2/CSVFileParserTest.java
@@ -3,9 +3,11 @@ package assignment2;
 import assignment2.model.CSVFileParser;
 import assignment2.model.CancellationReason;
 import assignment2.model.CancelledTransaction;
+import assignment2.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +17,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class CSVFileParserTest {
 
     private CSVFileParser csvFileParser;
+
+    private static String inventoryPath1 = "src/test/resources/test_inventory2.json";
+    private static String testInventoryCSVPath = "src/test/resources/test_inventory1.csv"; 
+    private static String testSellerTransactionCSVPath = "src/test/resources/seller_transaction1.csv";
+
+    private static String testCashierTransactionCSVPath = "src/test/resources/cashier_transaction.csv";
 
     @Test
     public void updateAndGetCancelledTransaction(){
@@ -59,6 +67,127 @@ public class CSVFileParserTest {
         assertEquals(csvFileParser.getCancelledTransactions().size(), 0);
 
     }
+
+    //test for reading and writing inventory to CSV File
+    @Test
+    public void readAndWriteInventoryTest(){
+        JsonParser jp = new JsonParser(inventoryPath1, "src/test/resources/test_users3.json", "src/test/resources/InitialCash.json", "src/test/resources/credit_cards.json");
+        List<Product> inventory = jp.getInventory();
+        CSVFileParser csvFileParser = new CSVFileParser(testInventoryCSVPath, testSellerTransactionCSVPath);
+        InventoryModel inventoryModel = new InventoryModel(inventory, jp, csvFileParser);
+        inventoryModel.initializeProductsToString();
+        
+        //read from file
+        List<List<String>> inventoryRead = inventoryModel.getInventoryAsString();
+
+        //check for equivalent length
+        assertEquals(inventoryModel.getInventory().size(), inventoryRead.size());
+
+        //check if the content is equivalent
+
+        for (int i=0; i < inventoryModel.getInventory().size(); i++){
+            List<String> productString = inventoryRead.get(i);
+            assertEquals(5, productString.size());
+            
+            //name
+            assertEquals(inventoryModel.getInventory().get(i).getName(), productString.get(0));
+
+            //code
+            assertEquals(String.valueOf(inventoryModel.getInventory().get(i).getCode()), productString.get(1));
+
+            //category
+            assertEquals(inventoryModel.getInventory().get(i).getCategory(), productString.get(2));
+
+            //price
+            assertEquals(String.valueOf(inventoryModel.getInventory().get(i).getPrice()), productString.get(3));
+
+            //quantity
+            assertEquals(String.valueOf(inventoryModel.getInventory().get(i).getQuantity()), productString.get(4));
+
+        }
+        
+    }
+
+    //test for reading and writing to seller transactions   
+    @Test
+    public void readAndWriteSellerTransactionTest(){
+        JsonParser jp = new JsonParser(inventoryPath1, "src/test/resources/test_users3.json", "src/test/resources/InitialCash.json", "src/test/resources/credit_cards.json");
+        CSVFileParser csvFileParser = new CSVFileParser(testInventoryCSVPath, testSellerTransactionCSVPath, "", "", "", "src/test/resources/cash_available1.java");
+        List<Product> inventory = jp.getInventory();
+        
+        MainModel mainModel = new MainModel(jp, csvFileParser);
+
+        mainModel.setUser(mainModel.getLoginModel().login("test1", "pw"));
+
+        for (Product p: mainModel.getInventoryModel().getInventory()){
+            mainModel.getUser().getCart().put(p, 2);
+        }
+
+
+        assertEquals(mainModel.getCsvFileParser().readInventoryFromFile().size() ,mainModel.getUser().getCart().size());
+
+        int cartSize = mainModel.getUser().getCart().size();
+        mainModel.checkout("card");
+
+        List<List<String>> sellerTransactions = mainModel.getCsvFileParser().readSellerTransactions();
+
+        assertEquals(sellerTransactions, mainModel.getSellerTransactionAsString());
+
+        assertEquals(cartSize, sellerTransactions.size());
+
+        for(List<String> s: sellerTransactions){
+            assertEquals(2, Integer.parseInt(s.get(2)));
+        }
+
+
+
+        //reset
+        List<List<String>> emptyList = new ArrayList<List<String>>();
+        mainModel.getCsvFileParser().writeSellerTransactions(emptyList);
+        jp.updateInventory(inventory);
+    }
+
+    //test for reading and writing to cashier transactions
+    @Test
+    public void readAndWriteCashierTransactionTest(){
+        JsonParser jp = new JsonParser(inventoryPath1, "src/test/resources/test_users3.json", "src/test/resources/InitialCash.json", "src/test/resources/credit_cards.json");
+        CSVFileParser csvFileParser = new CSVFileParser(testInventoryCSVPath, "", testCashierTransactionCSVPath, "", "", "src/test/resources/cash_available1.java");
+        List<Product> inventory = jp.getInventory();
+        
+        MainModel mainModel = new MainModel(jp, csvFileParser);
+
+        mainModel.setUser(mainModel.getLoginModel().login("test2", "pw"));
+
+        for (Product p: mainModel.getInventoryModel().getInventory()){
+            mainModel.getUser().getCart().put(p, 2);
+        }
+
+
+        assertEquals(mainModel.getCsvFileParser().readInventoryFromFile().size() ,mainModel.getUser().getCart().size());
+
+        int cartSize = mainModel.getUser().getCart().size();
+        mainModel.checkout("card");
+
+        List<List<String>> cashierTransactions = mainModel.getCsvFileParser().readCashierTransactions();
+
+        assertEquals(cashierTransactions, mainModel.getCashierTransactionAsString());
+
+        assertEquals(1, cashierTransactions.size());
+
+        for(List<String> s: cashierTransactions){
+            assertEquals(5, s.size());
+        }
+
+
+
+        //reset
+        List<List<String>> emptyList = new ArrayList<List<String>>();
+        mainModel.getCsvFileParser().writeCashierTransactions(emptyList);
+        jp.updateInventory(inventory);
+
+    }
+
+
 
 
 
